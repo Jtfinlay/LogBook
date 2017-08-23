1bd8b14478a53149315118b8822e9bf11362c691
James Finlay <jtfinlay@ualberta.ca>
2017-08-22 21:52:18 -0700

    Force number in the model

--- app/models/timelog.js
+++ app/models/timelog.js
@@ -2,10 +2,10 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
     type: DS.attr(),
-    timestamp: DS.attr(),
-    elapsedtime: DS.attr(),
+    timestamp: DS.attr("number"),
+    elapsedtime: DS.attr("number"),
 
     timestampReadable: Ember.computed("timestamp", function() {
-        return new Date(Number(this.get("timestamp")));
+        return new Date(this.get("timestamp"));
     })
 });
