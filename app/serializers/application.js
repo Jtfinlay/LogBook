1077fb893c3b26198904535f0599cfe205fc964d
James Finlay <jtfinlay@ualberta.ca>
2017-08-29 22:01:45 -0700

    Working serializer to convert server data to json api format

---
+++ app/serializers/application.js
@@ -0,0 +1,20 @@
+import DS from 'ember-data';
+
+export default DS.JSONAPISerializer.extend({
+    serialize(snapshot, options) {
+        var json = this._super(...arguments);
+        return json;
+    },
+    normalizeResponse(store, primaryModelClass, payload, id, requestType) {
+        for (var index = 0; index < payload.data.length; ++index)
+        {
+            let data = payload.data[index];
+            payload.data[index] = {attribute: data};
+            payload.data[index].id = data.id;
+            payload.data[index].type = "timelog";
+        }
+        
+        let json = this._super(...arguments);
+        return json;
+    }
+});
