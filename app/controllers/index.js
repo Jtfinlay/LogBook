3113cce91d174fdedc5243df03eb91d770ca2cd4
James Finlay <jtfinlay@ualberta.ca>
2017-10-07 18:32:44 -0700

    bug fix

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,14 +4,14 @@ export default Ember.Controller.extend({
 
     actions: {
         submitTime: function(timeElapsed) {
-            var timeLog = this.get('store').createRecord('timelog', {
-                elapsedtime: timeElapsed
+            var timeLog = this.get('store').createRecord('entry', {
+                elapsedTime: timeElapsed
             });
             function success(post) {
                 console.log('success');
             }
             function failure(reason) {
-                console.log('failure');
+                console.log(reason);
             }
             timeLog.save().then(success).catch(failure);
         }
