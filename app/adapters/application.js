17fce13ff77462084ac17f15d1aab589293b88ab
James Finlay <jtfinlay@ualberta.ca>
2017-10-07 19:31:39 -0700

    Point to localhost and set content type

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,4 +2,8 @@ import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
     namespace: 'api',
+    host: 'http://localhost:10010',
+    headers: {
+        "Content-Type": 'application/json'
+    }
 });
